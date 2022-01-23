064fb63223e737157448ba05e92248b778b816b7
ErickDiaz <201424@ids.upchiapas.edu.mx>
2022-01-22 18:21:01 -0600

    Create component Login

--- primerComponente/urls.py
+++ primerComponente/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, re_path
 from django.conf.urls import include
 
+
 from primerComponente.views import PrimerTablaList
 
 urlpatterns = [
-    re_path(r'^primer_componente/$', PrimerTablaList.as_view()),
+    re_path(r'^lista/$', PrimerTablaList.as_view()),    
 ]
